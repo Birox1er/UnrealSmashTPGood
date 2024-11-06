09cf3085ecbe9a24de950475115d5f8034a6f73c
Emile Birraux <Emile.birraux@edu.devinci.fr>
2024-11-06 11:23:00 +0100

    It's commit time 6

--- SmashUE_ClearedVersion-master/Source/SmashUE/Public/Characters/SmashCharacter.h
+++ SmashUE_ClearedVersion-master/Source/SmashUE/Public/Characters/SmashCharacter.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Character.h"
 #include "SmashCharacter.generated.h"
 
+class USmashCharacterStateMachine;
+
 UCLASS()
 class SMASHUE_API ASmashCharacter : public ACharacter
 {
@@ -35,5 +37,12 @@ protected:
 	UPROPERTY(BlueprintReadOnly)
 	float OrientX=1.f;
 	void RotateMeshUsingOrientX()const;
+#pragma endregion
+#pragma region State Machine
+public:
+	void CreateStateMachine();
+	void InitStateMachine();
+protected:
+	TObjectPtr<USmashCharacterStateMachine> StateMachine;
 #pragma endregion
 };
