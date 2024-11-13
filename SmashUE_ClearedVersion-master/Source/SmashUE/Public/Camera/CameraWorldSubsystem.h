011263cf173bea8b420b86446e7e59be6d636bc6
Emile Birraux <Emile.birraux@edu.devinci.fr>
2024-11-13 21:25:37 +0100

    Regions

--- SmashUE_ClearedVersion-master/Source/SmashUE/Public/Camera/CameraWorldSubsystem.h
+++ SmashUE_ClearedVersion-master/Source/SmashUE/Public/Camera/CameraWorldSubsystem.h
@@ -14,19 +14,35 @@ UCLASS()
 class SMASHUE_API UCameraWorldSubsystem : public UTickableWorldSubsystem
 {
 	GENERATED_BODY()
+#pragma region Subsytems Overrides
 	public:
 	virtual void PostInitialize() override;
 	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
 	virtual void Tick(float DeltaTime) override;
+protected:
+#pragma endregion
+#pragma region Main Camera
+	public:
 	virtual TStatId GetStatId() const override{return TStatId();};
+protected:
+	UPROPERTY()
+	TObjectPtr<UCameraComponent> CameraMain;
+#pragma endregion
+#pragma region Follow Target
+	public:
 	void AddFollowTarget(UObject* Target);
 	void RemoveFollowTarget(UObject* Target);
 protected:
 	UPROPERTY()
-	TObjectPtr<UCameraComponent> CameraMain;
-	UPROPERTY()
 	TArray<UObject*> FollowTargets;
-	void TickUpdateCameraPosition(float DeltaTime);
 	FVector CalculateAveragePositionBetweenTargets();
+#pragma endregion
+#pragma region Misc
+public:
+protected:
 	UCameraComponent* FindCameraByTag(const FName& Tag) const;
+	void TickUpdateCameraPosition(float DeltaTime);
+#pragma endregion
+	#pragma region Bounds
+
 };
